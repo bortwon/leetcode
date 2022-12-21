ef020c42b302a7322a3c3da85840c73140e58fef
bortw <pikanicka@gmail.cim>
2022-12-22 01:52:01 +0300

    383. Ransom Note Easy

--- tasks.py
+++ tasks.py
@@ -16,3 +16,25 @@ class Solution(object):
                 res += d[s[i]]
         return res
 
+
+# 383. Ransom Note
+# Easy
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        d = {}
+        for i in magazine:
+            if i not in d:
+                d[i] = 1
+            else:
+                d[i] += 1
+        for ch in ransomNote:
+            if ch not in d or d[ch] == 0:
+                return False
+            else:
+                d[ch] -= 1
+        return True
