cfc35d0d3940d329b208b86727b7fa4ff49c8c50
bortw <pikanicka@gmail.cim>
2022-12-22 01:52:52 +0300

    876. Middle of the Linked List Easy

--- tasks.py
+++ tasks.py
@@ -59,3 +59,21 @@ class Solution(object):
             else:
                 res.append(str(i))
         return res
+
+
+
+# 876. Middle of the Linked List
+# Easy
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        tmp = head
+        while tmp and tmp.next:
+            head = head.next
+            tmp = tmp.next.next
+        return head
