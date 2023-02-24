dbe83bf104b9cbb87a18881a36cbc96c58bf17d2
bortw <pikanicka@gmail.cim>
2023-02-24 21:47:44 +0300

    14. Longest Common Prefix. Easy

--- tasks.py
+++ tasks.py
@@ -155,3 +155,38 @@ class Solution(object):
         return str(x) == str(x)[::-1]
 
 
+
+# 14. Longest Common Prefix
+# Easy
+
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+#
+# Example 1:
+#
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+
+# Example 2:
+#
+# Input: strs = ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        res = ''
+        length = min(map(lambda x: len(x), strs))
+        for i in range(length):
+            if len(set(map(lambda x: x[i], strs))) == 1:
+                res += strs[0][i]
+                continue
+            break
+        return res
+
+
