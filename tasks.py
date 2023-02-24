e87031f1f59ec0da104e7ad5ba59d952428899a4
bortw <pikanicka@gmail.cim>
2023-02-24 23:01:15 +0300

    7. Reverse Integer. Medium

--- tasks.py
+++ tasks.py
@@ -296,4 +296,46 @@ class Solution(object):
 
 
 
+# 7. Reverse Integer
+# Medium
+
+# Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the
+# signed 32-bit integer range [-231, 231 - 1], then return 0.
+#
+# Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+#
+# Example 1:
+#
+# Input: x = 123
+# Output: 321
+
+# Example 2:
+#
+# Input: x = -123
+# Output: -321
+
+# Example 3:
+#
+# Input: x = 120
+# Output: 21
+
+
+
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        ex = (-(2 ** 31), 2 ** 31 - 1)
+        if x < 0:
+            x = int(str(x)[1:][::-1]) * -1
+            return 0 if x < ex[0] else x
+        if x > 0:
+            x = int(str(x)[::-1])
+            return 0 if x > ex[1] else x
+        return 0
+
+
+
 #
